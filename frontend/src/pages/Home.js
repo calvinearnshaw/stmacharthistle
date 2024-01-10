b0660a1de718432a0f5e5fc6587cb18f5f13a217
calvinearnshaw <calvin.earnshaw@outlook.com>
2024-01-10 23:38:19 +0000

    Added homepage, next task is to start work on backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import {FaPlaneDeparture} from "react-icons/fa";
 import {Link} from "react-router-dom";
 import {IoIosArrowForward} from "react-icons/io";
 
+
+
 const Home = () => {
     return (
         <div className="homepage">
@@ -9,10 +11,6 @@ const Home = () => {
                 <img src={require('../static/Images/heroimg.jpg')} alt="hero" />
                 <h1>Since 2023.</h1>
                 <h2>The home of St Machar Thistle AFC.</h2>
-            </div>
-            <div className="sponsors">
-                <h2>St Machar Thistle are proudly sponsored by</h2>
-                <img src={require('../static/Images/sponsors24.png')} alt="sponsors" />
             </div>
             <div className="fixtures">
                 <h2>Upcoming Fixtures</h2>
@@ -51,12 +49,118 @@ const Home = () => {
                     <IoIosArrowForward className={"arrow-fwd"} />
                 </Link>
             </div>
+            <div className="league-table">
+                <h2>Aberdeenshire AFA Division 2 East</h2>
+                <table className="lg-t">
+                    <tbody>
+                        <tr>
+                            <th>Pos</th>
+                            <th>Club</th>
+                            <th>P</th>
+                            <th>W</th>
+                            <th>D</th>
+                            <th>L</th>
+                            <th>GD</th>
+                            <th>Pts</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Monymusk</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>FC Polska</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Meldrum United</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>AC Mill Inn Academy</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>Laurencekirk West End</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>Ellon Amateurs</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr className="lgPos">
+                            <td>7</td>
+                            <td>St Machar Thistle</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>Ferryhill</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Byron Sports Club</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>-</td>
+                            <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <div className="latest-news">
                 <span>
                     <h2>Latest News</h2>
                     <Link to={"/news"} className={"news-link"}>
-                        <p>All News</p>
-                        <IoIosArrowForward className={"arrow-fwd"} />
+                        <IoIosArrowForward size={'30px'} color={'#851B87'} className={"arrow-fwd"} />
                     </Link>
                 </span>
                 <div className="entries">
