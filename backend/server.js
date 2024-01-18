babc7cc70639a03e0afb4c7a6f4facc1b2499b1d
calvinearnshaw <calvin.earnshaw@outlook.com>
2024-01-18 14:02:32 +0000

    Reconnecting to MDB, patch

--- backend/server.js
+++ backend/server.js
@@ -38,17 +38,13 @@ app.use((req, res, next) => {
 app.use("/api/league", leagueRoute);
 
 // Connect to MongoDB and start server. We don't start the server unless we successfully connect to db.
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => {
-//         // Listen for requests
-//         app.listen(process.env.PORT, () => {
-//             console.log('Connected to MongoDB & listening on port', process.env.PORT);
-//         });
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     });
-
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT);
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to MongoDB & listening on port', process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
